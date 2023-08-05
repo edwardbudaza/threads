295a56d864872cbd6850126a5f6c47b0c96f01d8
edwardbudaza <edward.budaza@yahoo.co.za>
2023-08-05 04:17:49 +0200

    first commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+    title: 'threads',
+    description: 'A Meta Threads Web Application'
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode} ) {
+    return (
+        <ClerkProvider>
+            <html lang='en'>
+                <body className={`${inter.className} bg-dark-1`}>
+                    {children}
+                </body> 
+            </html>
+        </ClerkProvider>
+    );
+}
