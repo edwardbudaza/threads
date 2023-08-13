9d4f7308e699f37bee77257680c399f5d87144a5
edwardbudaza <edward.budaza@yahoo.co.za>
2023-08-13 07:56:15 +0200

    starting of new day and new dev environment

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+
+interface Props {
+    userId: string;
+}
+
+function PostThread({ userId }: Props) {
+  return (
+    <div>PostThread</div>
+  )
+}
+
+export default PostThread
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,18 +2,24 @@
 
 import { FilterQuery, SortOrder } from "mongoose";
 import { revalidatePath } from "next/cache";
+
 import User from "../models/user.model";
+import Community from "../models/community.model";
 
 import { connectToDB } from "../mongoose";
 
-// export async function fetchUser(userId: string) {
-//     try {
-//         connectToDB();
-        
-//     } catch (error: any) {
-//         throw new Error(`Failed to fetch user: ${error.message}`);
-//     }
-// }
+export async function fetchUser(userId: string) {
+    try {
+        connectToDB();
+
+        return await User.findOne({ id: userId }).populate({
+            path: "communities",
+            model: Community,
+        }); 
+    } catch (error: any) {
+        throw new Error(`Failed to fetch user: ${error.message}`);
+    }
+}
 
 interface Params {
     userId: string;
